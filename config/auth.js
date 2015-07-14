217e47b4d0351b225d4f91778ac9c95f6a77c3da
vig9295 <vig9295@gmail.com>
2015-07-13 22:30:16 -0700

    Deployment

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1599664560285645', // your App ID
         'clientSecret'  : 'd92e033cd411da2db3f12573d41c2b09', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : '/auth/facebook/callback'
     }
 }
