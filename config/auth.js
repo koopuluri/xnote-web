35c13c4a354a2a03e91aa40ae140a03ae3269917
Karthik Uppuluri <koopuluri@Karthiks-MacBook-Pro.local>
2015-07-21 16:31:15 -0700

    auth

--- config/auth.js
+++ config/auth.js
@@ -1,19 +1,18 @@
-// module.exports = {
-//     'facebookAuth' : {
-//         'clientID'      : '1598121863773248',
-//         //'1599664560285645' // your App ID
-//         'clientSecret'  : '8a87f519f34268f7b3fc0a82d7ee1062',
-//         //''d92e033cd411da2db3f12573d41c2b09' // your App Secret
-//         'callbackURL'   : '/auth/facebook/callback'
-//     }
-// }
-
-
-// for testing;
 module.exports = {
-	'facebookAuth': {
-		'clientID': '1598123567106411',
-		'clientSecret': '3a53103c20019511924fa0d411a25944',
-		'callbackURL': '/auth/facebook/callback'
-	}
+    'facebookAuth' : {
+        'clientID'      : '1598121863773248',
+        //'1599664560285645' // your App ID
+        'clientSecret'  : '8a87f519f34268f7b3fc0a82d7ee1062',
+        //''d92e033cd411da2db3f12573d41c2b09' // your App Secret
+        'callbackURL'   : '/auth/facebook/callback'
+    }
 }
+
+
+// // for testing;
+// module.exports = {
+// 	'facebookAuth': {
+// 		'clientID': '1598123567106411',
+// 		'clientSecret': '3a53103c20019511924fa0d411a25944',
+// 		'callbackURL': '/auth/facebook/callback'
+// 	}
